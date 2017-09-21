2cd718e1b3e52c1d68751e5a0cbf316d43e2a32d
bhaskardonadapti5 <30465848+bhaskardonadapti5@users.noreply.github.com>
2017-09-21 15:42:59 +0530

    committing the onActivityResult codes

--- app/src/main/java/com/example/bdondapati/customdocumentbrowser/MainActivity.java
+++ app/src/main/java/com/example/bdondapati/customdocumentbrowser/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by BDondapati on 21-09-2017.
  */
 
 public class MainActivity extends AppCompatActivity {
+
+    private int DOCUMENT_REQUEST_CODE = 100;
 
     private Button mPickDocuments;
 
@@ -26,9 +29,29 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 Intent intent = new Intent(MainActivity.this, CustomFileBrowserActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, DOCUMENT_REQUEST_CODE);
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == DOCUMENT_REQUEST_CODE  )
+        {
+            if(resultCode==RESULT_OK)
+            {
+                String documentURI = data.getStringExtra("resultURI");
+
+                Toast.makeText(MainActivity.this, "Selected Document Path at:: " + documentURI , Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                Toast.makeText(MainActivity.this, "Document Selection is cancelled by user" , Toast.LENGTH_LONG).show();
+            }
+        }
 
     }
 }
